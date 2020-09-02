8c43a8639ce4fbac4ea9516fbc9aa0b08f3891c7
SangHyo <gilsport1@gmail.com>
2020-09-02 12:29:19 +0900

    url,os,querystring module

--- node.server.js
+++ node.server.js
@@ -3,7 +3,12 @@ let http = require("http");
 http
   .createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("<h1>Hello world...!</h1>");
+    
+    response.write("<h1>Hello world...!</h1>");
+    response.write("<hr>");
+    response.end("hello world");
+    
+
   })
   .listen(52273, function () {
     console.log("Server running at http://127.0.0.1:52273/");
